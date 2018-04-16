4c244db907ce801dc8242d2b7e7b77017c0ba957
Pernille Klevstuen <peklevstuen@gmail.com>
2018-04-16 16:17:33 +0200

    Studentpage

--- melon/models.py
+++ melon/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
-""" 
+
+from django.db import models
+
+
+class Subscribe(models.Model):
+    subscribe = models.IntegerField(default=0)
+
+"""
 https://alexpnt.github.io/2017/07/15/django-calendar/
 
 class Event(models.Model):
@@ -13,7 +20,7 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
         overlap = False
         if new_start == fixed_end or new_end == fixed_start:
@@ -23,11 +30,11 @@ class Event(models.Model):
         elif new_start <= fixed_start and new_end >= fixed_end:
             overlap = True
         return overlap
-    
+
     def clean(self):
         if self.end_time <= self.start_time:
             raise ValidationError('End time must be after start time.')
-        
+
         events = Event.objects.filter(day=self.day)
         if events.exists():
             for event in events:
